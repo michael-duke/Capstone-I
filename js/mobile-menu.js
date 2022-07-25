bc9071487b1ffc696b8cd1c782453eb39b7ef789
michael-duke <michael.21duke@gmail.com>
2022-07-25 09:38:03 +0300

    Update package.json stylelint according to Microverse linters.yml

---
+++ js/mobile-menu.js
@@ -0,0 +1,9 @@
+const toggleMenu = (e) => {
+  const menuBtn = document.querySelector('.menu-btn');
+  const { target } = e;
+  if(target.nodeName==='SPAN' || target.nodeName==='DIV')
+  menuBtn.classList.toggle('open');
+  console.log(menuBtn.nodeName);
+};
+
+export default toggleMenu;
